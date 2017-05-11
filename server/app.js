c437bf3848d4b77fac015119d11ae6000f433d20
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-11 19:16:58 -0400

    set up server and test commands

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/vendor', express.static(path.join(__dirname, '..', 'node_modules')));
+app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'index.html')));
+app.get('/index', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'index.html')));
+
+module.exports = app;
