67959f2c855948c403bbdc062281053f2395bcfb
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-11 19:22:53 -0400

    set up favicon

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const app = express();
 
 app.use('/vendor', express.static(path.join(__dirname, '..', 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
+app.use(favicon(path.join(__dirname, '..', 'favicon.ico')));
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'index.html')));
 app.get('/index', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'index.html')));
-
 module.exports = app;
