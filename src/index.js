57ac83cf621f4b87cac59ac952b938345346b9b6
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-11 19:44:50 -0400

    added brace instead of ace

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log(123);
+import JSONEditor from 'jsoneditor';
+import ace from 'brace';
+var container = document.getElementById('root');
+var options = {
+  modes: ['text', 'code', 'tree', 'form', 'view'],
+  mode: 'code',
+  ace: ace
+};
+var json = {
+  array: [1, 2, 3],
+  boolean: true,
+  null: null,
+  number: 123,
+  object: {a: 'b', c: 'd'},
+  string: 'Hello World'
+};
+var editor = new JSONEditor(container, options, json);
