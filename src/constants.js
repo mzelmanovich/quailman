dd2d4a4c92b929a847d3b15c7077938158ff5b8c
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-12 18:32:01 -0400

    renamed constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SET_AUTHENTICATION = 'SET_AUTHENTICATION';
+export const ADD_CHOP = 'ADD_CHOP';
+export const ADD_RESULT = 'ADD_RESULT';
+export const CHANGE_FUNCTION = 'CHANGE_FUNCTION';
+export const CHANGE_PORK_TYPE = 'CHANGE_PORK_TYPE';
