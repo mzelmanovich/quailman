00f7e9433e755542f218fd12a458512f678fc10d
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-13 10:55:38 -0400

    forced pannel height

--- src/components/PorkChop.jsx
+++ src/components/PorkChop.jsx
@@ -14,19 +14,19 @@ const PorkChop = ({index, chop, selector}) => {
       </div>);
   }
   return (<div className="col-xs-6 col-md-4 col-lg-4">
-    <div>
+      <Pannel title={'Chop: ' + (index + 1)}>
   <ul className="nav nav-tabs" role="tablist">
     <li role="presentation" className="active"><a href={`#inputs${index}`} aria-controls={`inputs${index}`} role="tab" data-toggle="tab">Inputs</a></li>
     <li role="presentation"><a href={`#code${index}`}  aria-controls={`code${index}`}  role="tab" data-toggle="tab">Code</a></li>
     <li role="presentation"><a href={`#result${index}`}  aria-controls={`result${index}`}  role="tab" data-toggle="tab">Result</a></li>
   </ul>
 
-  <div className="tab-content">
+  <div className="tab-content" style={{height: '400px'}}>
     <div role="tabpanel" className="tab-pane fade in active" id={`inputs${index}`}><p>inputs</p></div>
     <div role="tabpanel" className="tab-pane fade" id={`code${index}`}><p>code</p></div>
     <div role="tabpanel" className="tab-pane fade" id={`result${index}`}><p>result</p></div>
   </div>
-  </div>
+ </Pannel>
 </div>);
 };
 
